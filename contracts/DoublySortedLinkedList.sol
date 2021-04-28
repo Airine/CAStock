a350f78bbf2da1d7332b74a1dd8af92550301306
aaron <airine0119@gmail.com>
2021-04-28 11:22:50 +0800

    add definition of linked lists

---
+++ contracts/DoublySortedLinkedList.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <=0.8.4;
+
+contract DoublySortedLinkedList {
+    
+}
---
+++ contracts/LinkedList.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <=0.8.4;
+
+contract LinkedList {
+
+    struct Node {
+        bytes data;
+        uint next;
+    }
+
+    
+
+}
