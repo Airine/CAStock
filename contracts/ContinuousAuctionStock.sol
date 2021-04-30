336bcaac6e968138cd9d5da757a5618339d78b20
aaron <airine0119@gmail.com>
2021-04-30 18:28:39 +0800

    complete buy and sell logic

--- contracts/ContinuousAuctionStock.sol
+++ contracts/ContinuousAuctionStock.sol
@@ -8,13 +8,17 @@ import "./DoublySortedLinkedList.sol";
 struct BuyRequest {
     uint256 price;
     uint256 stock;
-    string status;
+    uint256 amount; // fixed value for record purpose
+    uint8 status; // 1: pending, 2: abort, 3: finished
+    address buyer;
 }
 
 struct SellRequest {
     uint256 price;
     uint256 stock;
-    string status;
+    uint256 amount; // fixed value for record purpose
+    uint8 status; // 1: pending, 2: abort, 3: finished
+    address seller;
 }
 
 contract ContinuousAuctionStock {
@@ -70,30 +74,123 @@ contract ContinuousAuctionStock {
             payable(buyer).transfer(unuse * 1 ether);    
         }
         
-        if (buyable > amount) {
-            stocks[buyer] += amount;
-            buyable -= amount;
-        } else {
-            emit log("no enough stock to buy");
-        }
-        // TODO: send the buy request
+        // if (buyable > amount) {
+        //     stocks[buyer] += amount;
+        //     buyable -= amount;
+        // } else {
+        //     emit log("no enough stock to buy");
+        // }
         
-        // require(buyer!=issuer, "the buyer can not be the issuer");
+        // TODO: send the buy request
+        uint requestID = buyReqs.length - 1;
+        buyReqs.push(BuyRequest(price, amount, amount, 1, buyer));
+        
+        // 1. if can be satisified, process directly
+        if (price > sellPrices.min()) {
+            uint id = sellPrices.findle(price);
+            while (id != 0) {
+                Queue q = sellPrices.getQ(id);
+                while (!q.empty()) {
+                    uint sellReqID = q.top();
+                    SellRequest storage request = sellReqs[sellReqID];
+                    if (request.status == 1) {
+                        address payable seller = payable(request.seller);
+                        if (request.stock > amount) {
+                            request.stock -= amount;
+                            buyReqs[requestID].status = 3;
+                            buyReqs[requestID].amount = 0;
+                            stocks[buyer] += amount;
+                            seller.transfer(price * amount * 1 ether);
+                            return;
+                        } else {
+                            amount -= request.stock;
+                            request.status = 3;
+                            seller.transfer(price * request.stock * 1 ether);
+                            request.stock = 0;
+                            q.dequeue();
+                        }
+                    }
+                }
+                uint prevID = sellPrices.getPrev(id);
+                sellPrices.updateQ(id, q);
+                id = prevID;
+            }
+        }
+        
+        // 2. else add the left amount to the queue
+        uint idx = buyPrices.find(price);
+        if (idx == 0) {
+            // not found
+            Queue newPriceQ = new Queue();
+            newPriceQ.enqueue(requestID);
+            buyPrices.insert(DataValue(newPriceQ, price));
+            emit log("new buy request price");
+        } else {
+            buyPrices.insertToQueue(idx, requestID);
+            emit log("add buy request to existing queue");
+        }
+        
     }
 
     function sell(uint amount, uint price) public {
         if (msg.sender == issuer) {
             count += amount;
-        } else {
-            address seller = msg.sender;
-            if (stocks[seller] < amount) {
-                emit log("no enough stock to sell");
-            } else {
-                payable(seller).transfer(price * amount * 1 ether);
-                stocks[seller] -= amount;
+        }
+        address seller = msg.sender;
+        if (msg.sender != issuer && stocks[seller] < amount ) {
+            emit log("no enough stock to sell");
+            return;
+        }
+        // payable(seller).transfer(price * amount * 1 ether);
+        if (seller != issuer) {
+            stocks[seller] -= amount;
+        }
+        uint requestID = sellReqs.length - 1;
+        sellReqs.push(SellRequest(price, amount, amount, 1, seller));
+        
+        // 1. if can be satisified, process directly
+        if (price < buyPrices.max()) {
+            uint id = buyPrices.findge(price);
+            while (id != 0) {
+                Queue q = buyPrices.getQ(id);
+                while (!q.empty()) {
+                    uint buyReqID = q.top();
+                    BuyRequest storage request = buyReqs[buyReqID];
+                    if (request.status == 1) {
+                        // address payable seller = payable(request.seller);
+                        if (request.stock > amount) {
+                            request.stock -= amount;
+                            sellReqs[requestID].status = 3;
+                            sellReqs[requestID].amount = 0;
+                            payable(seller).transfer(price * amount * 1 ether);
+                            return;
+                        } else {
+                            amount -= request.stock;
+                            request.status = 3;
+                            payable(seller).transfer(price * request.stock * 1 ether);
+                            request.stock = 0;
+                            q.dequeue();
+                        }
+                    }
+                }
+                uint nextID = sellPrices.getPrev(id);
+                buyPrices.updateQ(id, q);
+                id = nextID;
             }
         }
-        buyable += amount;
+                        
+        // 2. else add the left amount to the queue
+        uint idx = sellPrices.find(price);
+        if (idx == 0) {
+            // not found
+            Queue newPriceQ = new Queue();
+            newPriceQ.enqueue(requestID);
+            sellPrices.insert(DataValue(newPriceQ, price));
+            emit log("new sell request price");
+        } else {
+            sellPrices.insertToQueue(idx, requestID);
+            emit log("add sell request to existing queue");
+        }
     }
 
 }
--- contracts/Queue.sol
+++ contracts/Queue.sol
@@ -2,21 +2,30 @@
 pragma solidity >=0.4.25 <=0.8.4;
 
 contract Queue {
-    mapping(uint256 => bytes) queue;
+    mapping(uint256 => uint256) queue;
     uint256 first = 1;
     uint256 last = 0;
 
-    function enqueue(bytes memory data) public {
+    function enqueue(uint256 data) public {
         last += 1;
         queue[last] = data;
     }
+    
+    function top() public view returns (uint256) {
+        require(last >= first);  // non-empty queue
+        return queue[first];
+    }
 
-    function dequeue() public returns (bytes memory data) {
+    function dequeue() public returns (uint256 data) {
         require(last >= first);  // non-empty queue
 
         data = queue[first];
 
         delete queue[first];
         first += 1;
+    }
+    
+    function empty() public view returns (bool) {
+        return last < first;
     }
 }
